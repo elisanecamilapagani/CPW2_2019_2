c494612e38c43216abd3ed94336dd1c9cbedde56
Elisane Pagani <elisanecamila20@gmail.com>
2019-10-25 17:14:18 -0400

    base do sistema criado

--- prova01/js/index.js
+++
@@ -1,9 +0,0 @@
-function exibirMensagem() {
-    let nome = document.getElementById('nome').value;
-    let sobrenome = document.getElementById('sobrenome').value;
-    let mensagem = nome&&sobrenome ? `Palavras concatenadas: ${nome}${sobrenome}!` : '';
-
-    
-    document.getElementById('mensagem').innerHTML = mensagem;
-        
-}
